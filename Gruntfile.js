1b4055ea77b897137de203ffef99436a9930dd3f
Theo Nicolaou <theo@theo-nicolaou.co.uk>
2015-01-02 18:18:43 +0000

    Add csslint configuration (WIP), add separate lintcss task, add comments in file

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,12 @@ module.exports = function (grunt) {
     
 			grunt.initConfig({
   		
+  		//clean main compiled css file and minified version.
   		clean: {
     		css: ['css/tn.css', 'css/tn.min.css']
 			},
   		
+  		//compile SCSS files into CSS.
   		sass: {
     		dist: {
       		files: {
@@ -19,6 +21,7 @@ module.exports = function (grunt) {
       	}
   		},
   		
+  		//lint main CSS file.
   		csslint: {
   			strict: {
     			options: {
@@ -26,14 +29,17 @@ module.exports = function (grunt) {
     			},
     			src: ['css/tn.css']
   			},
+/*
   			lax: {
     			options: {
       			import: false
     			},
     			src: ['css/tn.css']
-  			}
+  			},
+*/
 			},
-
+			
+			//watch for changes to SCSS files.
   		watch: {
     		sass: {
       		files: ['scss/*/*.scss'],
@@ -41,6 +47,7 @@ module.exports = function (grunt) {
     		},
     	},
     	
+    	//concatenate all CSS files with .css extension in "css" folder.
     	concat: {
   			css: {
     			src: ['css/*.css'],
@@ -48,6 +55,7 @@ module.exports = function (grunt) {
   			}
 			},
 			
+			//minify main CSS file.
     	cssmin: {
   			target: {
     			files: [{
@@ -59,17 +67,66 @@ module.exports = function (grunt) {
     			}]
   			}
 			},
-			
-    	notify_hooks: {
-    		options: {
-      		enabled: true,
-      		title: "Grunt tasks completed",
-      		success: true,
-      		duration: 5
-    		}
+  		
+  		//configurations for OS X notifications, for each task.
+  		notify: {
+  			clean: {
+  				options: {
+  				  title: "Clean",
+      			message: "CSS file clean complete",
+      			success: true,
+      			duration: 5	
+  				}
+  			},
+  			
+  			sass: {
+  				options: {
+  				  title: "SCSS Compilation",
+      			message: "SCSS compilation complete",
+      			success: true,
+      			duration: 5	
+  				}
+  			},
+  			
+  			watch: {
+  				options: {
+  				  title: "Watch",
+      			message: "SCSS Watch in progress",
+      			success: true,
+      			duration: 5	
+  				}
+  			},
+  			
+  			concat: {
+  				 	options: {
+  				 	title: "CSS file concatenation",
+      			message: "CSS files concatenated",
+      			success: true,
+      			duration: 5
+  				}
+  			},
+  			
+  			cssmin: {
+  				  options: {
+  				  title: "CSS file minification",
+      			message: "CSS files minified",
+      			success: true,
+      			duration: 5
+  				}
+  			},
+  			
+  			csslint: {
+  				options: {
+      			title: "CSS Lint",
+      			message: "CSS lint complete",
+      			success: true,
+      			duration: 5
+    			}
+  			}
   		}	  		
   });
-
-	grunt.registerTask('develop', ['clean', 'sass', 'watch', 'csslint', 'notify_hooks']);
-	grunt.registerTask('build', ['clean', 'sass', 'concat:css', 'cssmin', 'notify_hooks']);
+  
+	grunt.registerTask('develop', ['clean', 'notify:clean', 'sass', 'notify:sass', 'watch', 'notify:watch']);
+	grunt.registerTask('lintcss', ['csslint', 'notify:csslint']);
+	grunt.registerTask('build', ['clean', 'notify:clean', 'sass', 'notify:sass', 'concat:css', 'notify:concat', 'cssmin', 'notify:cssmin']);
 };
